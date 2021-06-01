15e16683a900592ae496b1eae8fde957d4e7cdef
Clifford Laserre <claserre9@gmail.com>
2021-06-01 00:07:01 -0400

    Templating (Twig)

---
+++ var/81/81470971058e3cd36f3dda5a0a6d5ebc1336e572868feb4aa43d93f5674a0b83.php
@@ -0,0 +1,72 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* hello.html.twig */
+class __TwigTemplate_3dadbf3767aaebcdb0ec84c7a1c0aee1b3d8adfceeb5b7ed9f6bda62771049cd extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>Title</title>
+</head>
+<body>
+<h1>";
+        // line 8
+        echo twig_escape_filter($this->env, ($context["name"] ?? null), "html", null, true);
+        echo "</h1>
+
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "hello.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 8,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "hello.html.twig", "C:\\Users\\talma\\PhpstormProjects\\phpslim\\templates\\hello.html.twig");
+    }
+}
