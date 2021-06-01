54a9386cb12c192c0ab20c80d395f6f027f29998
Clifford Laserre <claserre9@gmail.com>
2021-05-31 23:56:22 -0400

    Templating (Mustache)

--- public/index.php
+++ public/index.php
@@ -1,21 +1,31 @@
 <?php
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . "/../vendor/autoload.php";
 
-$app = AppFactory::create();
+$container = new Container();
 
-$app->get("/", function (Request $request, Response $response) {
-    $response->getBody()->write("Hello, world !");
-    return $response;
+$container->set('templating', function () {
+    return new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates',
+            ['extension' => '']
+        )
+    ]);
 });
 
-$app->get("/{name}", function (Request $request, Response $response, array $args) {
-    $name = ucfirst($args['name']);
-    $response->getBody()->write("Hello, $name !");
+AppFactory::setContainer($container);
+
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $html = $this->get('templating')->render('hello.html', ['name' => $name]);
+    $response->getBody()->write($html);
     return $response;
 });
 
